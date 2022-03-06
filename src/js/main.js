6f74bfb6cdf12c3d0c6acbbd93397f1fa8ea12da
fajarfadillahh <fajaragustian979@gmail.com>
2022-03-06 20:45:09 +0700

    add scroll section active header__link

--- src/js/main.js
+++ src/js/main.js
@@ -13,9 +13,30 @@ function showMenu(menuId, toggleId) {
 }
 showMenu("header-menu", "header-toggle");
 
-// ===== STICKY HEADER =====
-const scrollY = window.pageYOffset;
+// ===== SCROLL SECTION ACTIVE HEADER-LINK =====
+const sections = document.querySelectorAll("section[id]");
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(`.header__menu a[href*= ${sectionId} ]`)
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(`.header__menu a[href*= ${sectionId} ]`)
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+// ===== STICKY HEADER =====
 function stickyHeader() {
   const header = document.getElementById("header");
 
